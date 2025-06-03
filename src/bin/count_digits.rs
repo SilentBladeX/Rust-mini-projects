e562ca432d2049ed03fb7ce5234b2714cd2cf2ee
Silent Blade X <muhammadahad0110@gmail.com>
2025-06-03 14:43:20 +0500

    update

--- src/bin/count_digits.rs
+++ src/bin/count_digits.rs
@@ -10,16 +10,15 @@ fn main() {
 
     let num: i32 = num.trim().parse().expect("failed to read number");
 
-    let x=cnt_fun(num);
-    println!("Total digits are {} ",x);
+    let x = cnt_fun(num);
+    println!("Total digits are {} ", x);
 }
 
-fn cnt_fun(mut num:i32)->i32{
-    let mut count=0;
-    while num>0{
-        let _digit=num%10;
-        count+=1;
-        num=num/10;
+fn cnt_fun(mut num: i32) -> i32 {
+    let mut count = 0;
+    while num > 0 {
+        count += 1;
+        num = num / 10;
     }
     count
 }
