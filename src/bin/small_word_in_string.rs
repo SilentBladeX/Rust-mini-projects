1da5fe057557fa8c675f70bbd3415ed678c94d8f
Silent Blade X <muhammadahad0110@gmail.com>
2025-06-26 21:44:21 +0500

    smallest word

--- src/bin/small_word_in_string.rs
+++ src/bin/small_word_in_string.rs
@@ -1,57 +1,42 @@
 use std::io;
 
-fn main(){
-
+fn main() {
     println!("Enter a String");
-    let mut str=String::new();
+    let mut str = String::new();
 
-    io::stdin()
-    .read_line(&mut str)
-    .expect("Failed to read smg");
+    io::stdin().read_line(&mut str).expect("Failed to read smg");
 
-    let str:String=match str.trim().parse(){
-        Ok(str)=>str,
-        Err(_)=>{
-            println!("Invalid string");
-            return;
-        }
-    };
+    let str = str.trim().to_string();
 
-
-    let small=short_word(&str);
-    println!("The Shortest word in String are : {small}");
-
+    let small = short_word(&str);
+    println!("The Smallest word is: {small}");
 }
 
-fn short_word(s: &String)->&str{
+fn short_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    let mut _len = 0;
+    let mut min_length = usize::MAX;
+    let mut short = None;
+    let mut start = 0;
 
-    let bytes=s.as_bytes();
-    let mut _len=0;
-    let mut  min_length=usize::MAX;
-    let mut short=None;
-    let mut start=0;
-
-    for (i,item) in bytes.iter().enumerate(){
-
-        if *item==b' '{
-            _len=i-start;
-            if _len<min_length{
-                min_length=_len;
-                short=Some(&s[start..i]);
+    for (i, item) in bytes.iter().enumerate() {
+        if *item == b' ' {
+            _len = i - start;
+            if _len < min_length {
+                min_length = _len;
+                short = Some(&s[start..i]);
             }
-            start=i+1;
-
+            start = i + 1;
         }
     }
 
-// Handle last word after final space
+    // Handle last word after final space
 
-    if start<s.len(){
-        _len=s.len()-start;
-        if _len<min_length{
-            short=Some(&s[start..]);
+    if start < s.len() {
+        _len = s.len() - start;
+        if _len < min_length {
+            short = Some(&s[start..]);
         }
-
     }
 
     return short.unwrap_or("");
